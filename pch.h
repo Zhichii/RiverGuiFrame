e731bd565a5fe31165c1a25cd81b3cbd6e788b45
HlHillMC <Qiuyixuan_last@outlook.com>
2023-05-25 07:31:00 +0800

    nothing updated

--- pch.h
+++ pch.h
@@ -9,6 +9,5 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #endif //PCH_H
